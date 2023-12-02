cf8222599894db638e1e9ee48d1fdcfe843bf4fa
Dijango Alves Rodolfo <dijango_ilha@hotmail.com>
2023-12-02 17:00:18 -0300

    fix: add service bind

--- src/framework/ioc/inversify.config.ts
+++ src/framework/ioc/inversify.config.ts
@@ -4,8 +4,10 @@ import { ModelModule } from './modelModule'
 import { UseCaseModule } from './useCaseModule'
 
 import { container } from '../shared/ioc/container'
+import { ServicesModule } from './servicesModule'
 
 container.load(RepositoryModule)
 container.load(ModelModule)
 container.load(UseCaseModule)
 container.load(OperatorModule)
+container.load(ServicesModule)
